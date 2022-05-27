731f409d0850f4a7fb7094e0995e7812a83570b8
HarshithaY <31440273+HarshithaY@users.noreply.github.com>
2022-05-27 16:56:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
+
 resource "google_compute_backend_service" "default" {
   project = var.project
   name    = var.backend
+  region   = var.region
+  protocol = var.ip_protocol
+  network  = var.network
+  connection_draining_timeout_sec = var.connection_draining_timeout_sec
+  session_affinity                = var.session_affinity
+  dynamic "backend" {
+    for_each = var.backends
+    content {
+      group       = lookup(backend.value, "group", null)
+      description = lookup(backend.value, "description", null)
+      failover    = lookup(backend.value, "failover", null)
+    }
+  }
   health_checks = [google_compute_http_health_check.default.id]
 }
 
