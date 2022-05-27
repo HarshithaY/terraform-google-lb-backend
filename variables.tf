21fd5468ce20155ba77dd41099b5e47a4e8f386f
HarshithaY <31440273+HarshithaY@users.noreply.github.com>
2022-05-27 14:50:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable "project" {
+  description = "The project to deploy to, if not set the default provider project is used."
+  default     = ""
+}
 
+variable "backend" {
+  description = "Backend service name"
+  default     = ""
+}
+
+variable "healthcheck" {
+  description = "Healthcheck name"
+  default     = ""
+}
