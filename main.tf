9579bcc35a733adba061e1b250a1fbc0dcdffc66
HarshithaY <31440273+HarshithaY@users.noreply.github.com>
2022-05-30 12:11:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,43 @@
+resource "google_compute_backend_service" "foobar" {
+  name        = "blablah"
+  description = "Hello World 1234"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+  enable_cdn  = false
 
-resource "google_compute_backend_service" "default" {
-  project = var.project
-  name    = var.backend
-  region   = var.region
-  protocol = var.ip_protocol
-  network  = var.network
-  connection_draining_timeout_sec = var.connection_draining_timeout_sec
-  session_affinity                = var.session_affinity
-  dynamic "backend" {
-    for_each = var.backends
-    content {
-      group       = lookup(backend.value, "group", null)
-      description = lookup(backend.value, "description", null)
-      failover    = lookup(backend.value, "failover", null)
-    }
+  backend {
+    group = "${google_compute_instance_group_manager.foo.instance_group}"
   }
-  health_checks = [google_compute_http_health_check.default.id]
+
+  health_checks = ["${google_compute_http_health_check.default.self_link}"]
+}
+
+resource "google_compute_instance_group_manager" "foo" {
+  name               = "terraform-test"
+  instance_template  = "${google_compute_instance_template.foobar.self_link}"
+  base_instance_name = "foobar"
+  zone               = "us-central1-f"
+  target_size        = 1
+}
+
+resource "google_compute_instance_template" "foobar" {
+  name         = "terraform-test"
+  machine_type = "n1-standard-1"
+
+  network_interface {
+    network = "default"
+  }
+
+  disk {
+    source_image = "debian-cloud/debian-8"
+    auto_delete  = true
+    boot         = true
+  }
 }
 
 resource "google_compute_http_health_check" "default" {
-  name               = var.healthcheck
+  name               = "test"
   request_path       = "/"
   check_interval_sec = 1
   timeout_sec        = 1
